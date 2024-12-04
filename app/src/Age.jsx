efae1df368f0cc092c4d8e47a593f319551f9588
Vittoria <vittoriatchotche@gmail.com>
2024-12-04 09:21:26 +0100

    ese conditional

---
+++ app/src/Age.jsx
@@ -0,0 +1,4 @@
+function Age({ age }) {
+  return age > 18 ? age : <h1>He is younger</h1>;
+}
+export default Age;
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,14 @@
 import { Hello } from "./Hello";
 
 import Welcome from "./Welcome";
+import Age from "./Age";
 
 function App() {
   return (
     <div>
       <Hello />
-      <Welcome name="Paul" age={38} />;
+      <Welcome name="Paul" age={38} />
+      <Age age={38} />
     </div>
   );
 }
