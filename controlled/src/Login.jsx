de6599cfc197503b92b6a80c4648e6e03b1b8948
Vittoria <vittoriatchotche@gmail.com>
2024-12-11 16:03:23 +0100

    contrtolled form

--- controlled/src/Login.jsx
+++ controlled/src/Login.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
 
-function Login() {
+function Login({ onLogin }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [remeber, setRemeber] = useState("");
-  const [login, setLogin] = useState("");
+  const [remember, setRemeber] = useState(false);
 
   function handleUsername(event) {
     const value = event.target.value;
@@ -19,18 +18,23 @@ function Login() {
     const value = event.target.value;
     setRemeber(value);
   }
-  function handleLogin() {
-    if (!username && !password) {
-      setLogin("");
+  function handleLogin(event) {
+    event.preventDefault();
+
+    if (!username || !password) {
+      alert("Username e Password non possono essere vuoti!");
+      return;
     }
+    // Se i campi sono validi, chiama la funzione onLogin passando i dati
+    onLogin(username, password, remember);
   }
 
   return (
     <div>
       <input name="username" onChange={handleUsername} value={username} />
       <input name="password" onChange={handlePassword} value={password} />
-      <input name="remeber" onChange={handleremember} value={remeber} />
-      <button onSubmit={handleLogin} value={login}>
+      <input name="remember" onChange={handleremember} value={remeber} />
+      <button onClick={handleLogin} value={login}>
         Login
       </button>
     </div>
